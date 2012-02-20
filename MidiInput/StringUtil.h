53574967e1dde9d15cd556480e922e79d78b2909
kbinani <kbinani@msn.com>
2012-02-21 01:03:15 +0900

    StringUtil::explodeに、エスケープ文字を指定できるようにした

--- MidiInput/StringUtil.h
+++ MidiInput/StringUtil.h
@@ -19,11 +19,11 @@ public:
      * @param limit 区切る回数の最大値
      * @return 区切られた文字列のリスト
      */
-    static vector<string> explode( string delimiter, string text, string::size_type limit = string::npos )
+    static vector<string> explode( string delimiter, string text, string::size_type limit = string::npos, string escape = "" )
     {
         vector<string> result;
         string::size_type searchFrom = 0;
-        string::size_type delimiterIndex = text.find( delimiter, searchFrom );
+        string::size_type delimiterIndex = getDelimiterIndex( text, delimiter, escape, searchFrom );
         while( delimiterIndex != string::npos ){
             string token = text.substr( searchFrom, delimiterIndex - searchFrom );
             result.push_back( token );
@@ -31,7 +31,7 @@ public:
             if( result.size() + 1 == limit ){
                 break;
             }
-            delimiterIndex = text.find( delimiter, searchFrom );
+            delimiterIndex = getDelimiterIndex( text, delimiter, escape, searchFrom );
         }
         result.push_back( text.substr( searchFrom ) );
         return result;
@@ -64,6 +64,42 @@ public:
 private:
     StringUtil()
     {
+    }
+
+    /**
+     * @brief 文字列から、エスケープ文字を考慮してデリミタ文字列の位置を探す
+     * @param text 文字列
+     * @param delimiter デリミタ(区切り)文字
+     * @param escape エスケープ文字
+     * @param searchFrom 検索開始インデックス
+     * @return デリミタ文字列の位置。見つからなければ string::npos を返す
+     */
+    static string::size_type getDelimiterIndex( string &text, string &delimiter, string &escape, string::size_type searchFrom )
+    {
+        if( escape.length() == 0 ){
+            return (int)text.find( delimiter, searchFrom );
+        }else{
+            while( searchFrom < text.length() ){
+                int draft = (int)text.find( delimiter, searchFrom );
+                if( draft == string::npos ){
+                    return string::npos;
+                }
+                int expectedEscapeIndex = draft - escape.length();
+                if( 0 <= expectedEscapeIndex ){
+                    // エスケープ文字が存在する可能性がある
+                    if( text.substr( expectedEscapeIndex, escape.length() ) == escape ){
+                        // エスケープ文字だった場合
+                        searchFrom = draft + 1;
+                    }else{
+                        return draft;
+                    }
+                }else{
+                    // エスケープ文字が存在し得ない
+                    return draft;
+                }
+            }
+            return string::npos;
+        }
     }
 };
 
